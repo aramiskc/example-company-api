99b04b78d31789ed44e51d913576b03750381023
“Nickhil <suryanscrit@gmail.com>
2023-07-17 12:54:47 -0500

    added controller methods and routes

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,64 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+    render :index 
+  end
+
+  def show
+    @employee = Employee.find_by(id: params[:id])
+    render :show
+  end
+
+  def create 
+    @employee = Employee.create(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      email: params["email"],
+      phone: params["phone"],
+      job_title: params["job_title"],
+      department_id: params["department_id"],
+    )
+    render :show
+  end
+
+  def update
+    @employee = Employee.find_by(id: params[:id])
+    @employee.update(
+      first_name: params["first_name"] || @employee.first_name,
+      last_name: params["last_name"] || @employee.last_name,
+      email: params["email"] || @employee.email,
+      phone: params["phone"] || @employee.phone,
+      job_title: params["job_title"] || @employee.job_title,
+      department_id: params["department_id"] || @employee.department_id
+    )
+    render :show 
+  end
+
+  def destroy
+    @employee = Employee.find_by(id: params[:id])
+    @employee.destroy 
+    render json: { message: "Record destroyed"}
+  end
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/employees" => "employees#index"
+  get "/employees/:id" => "employees#show"
+  post "/employees" => "employees#create"
+  patch "/employees/:id" => "employees#update"
+  delete "/employees/:id" => "employees#destroy"
 end
+
+
