8bb6b3899d49f873da1c6404c4f5f49acee9d357
“Nickhil <suryanscrit@gmail.com>
2023-07-17 12:00:57 -0500

    added seeds for database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_163219) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_164829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_163219) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.integer "phone"
+    t.string "phone"
     t.string "job_title"
     t.integer "department_id"
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Employee.create(first_name: "Bob", last_name: "Builder", email: "bob@builder.com", phone: "8008888767", job_title: "Builder", department_id: 1)
+# Employee.create(first_name: "Nick", last_name: "Broker", email: "nick@broker.com", phone: "8008845767", job_title: "CEO", department_id: 3)
+# Employee.create(first_name: "Eugene", last_name: "Crabs", email: "gene@lovesseafood.com", phone: "9003455456", job_title: "Crabber", department_id: 2)
+
+Department.create(name: "engineering", manager: "Micheal Scott", code: "ENG" )
+Department.create(name: "food", manager: "Dwight Schrute", code: "FD" )
+Department.create(name: "management", manager: "Bruce Wayne", code: "MGT" )
