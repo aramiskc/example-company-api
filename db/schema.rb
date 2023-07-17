cbef38493e64653fd31296948cda1f77ee351798
“Nickhil <suryanscrit@gmail.com>
2023-07-17 11:33:00 -0500

    added dept model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_162936) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_163219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.string "manager"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
